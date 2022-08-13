4649d1bc47b7c78cf4aa53c92f01f6faab42ed56
Alexander Shemetovskiy <alexkval@gmail.com>
2022-08-14 00:33:10 +0100

    Implement toggleReminder

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <Header title="Task Tracker" />
-    <Tasks :tasks="tasks" @delete-task="deleteTask" />
+    <Tasks :tasks="tasks" @toggle-reminder="toggleReminder" @delete-task="deleteTask" />
   </div>
 </template>
 
@@ -23,6 +23,9 @@ export default {
   methods: {
     deleteTask(id) {
       this.tasks = this.tasks.filter((t) => t.id !== id)
+    },
+    toggleReminder(id) {
+      this.tasks = this.tasks.map((t) => t.id == id ? { ...t, reminder: !t.reminder } : t)
     },
   },
   created() {
